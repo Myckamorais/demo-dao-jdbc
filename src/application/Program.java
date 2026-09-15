440d38d4400309e996953a197e34f157a635f388
Myckamorais <myckael2k@gmail.com>
2026-09-14 21:25:37 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,14 @@ package application;
 
 import model.entities.Department;
 
+import java.util.Date;
+
 public class Program {
     public static void main (String[] args){
 
         Department obj = new Department(1, "book");
-        System.out.println(obj);
+        
+        Seller seller  = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj );
+        System.out.println(seller);
     }
 }
