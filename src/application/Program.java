797d4c29fda4f2da74900793a704c313e6b5642e
Myckamorais <myckael2k@gmail.com>
2026-09-15 23:15:38 -0300

    Updade implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,8 +33,13 @@ public class Program {
         }
 
         System.out.println("\n=== TEST 4: seller Insert ====");
-        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! new ID: " + newSeller.getId());
+
+
+        System.out.println("\n=== TEST 5: seller update ====");
+        seller = sellerDao.findById(1);
+
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
